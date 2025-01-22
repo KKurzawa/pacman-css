2036f5fd3534e0afc71a61530b3f93546010c764
Kris Kurzawa <kurzawa1@yahoo.com>
2025-01-21 21:40:50 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,364 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <header className='header'>
+        <h1 className='high-score'>HIGH SCORE</h1>
+        <h1>16440</h1>
+      </header>
+      <main className='main'>
+
+        {/* top 1/3 */}
+        <article className='top-board'>
+
+          {/* top line */}
+          <section className='line-one'>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+          </section>
+
+          {/* 2nd line */}
+          <section className='line-two'>
+            <article className='line-two-a'>
+              <div className='dot'></div>
+              <div className='life'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='line-two-b'>
+            </article>
+            <article className='line-two-c'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='line-two-d'>
+            </article>
+            <article className='line-two-c'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='line-two-e'>
+            </article>
+            <article className='line-two-c'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='line-two-g'>
+            </article>
+            <article className='line-two-c'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='line-two-b'>
+            </article>
+            <article className='line-two-f'>
+              <div className='dot'></div>
+              <div className='life'></div>
+              <div className='dot'></div>
+            </article>
+          </section>
+
+          {/* 3rd line */}
+          <section className='line-one'>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+          </section>
+
+          {/* 4th line */}
+          <section className='line-four'>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='line-four-b'></div>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='line-four-c'></div>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='line-four-d'></div>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='line-four-e'></div>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='line-four-f'></div>
+            <div className='line-four-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+          </section>
+
+          {/* 5th line */}
+          <section className='line-one'>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+          </section>
+        </article>
+        <article className='bottom-board'>
+          {/* line one bottom  */}
+          <section className='line-one'>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+          </section>
+          <section className='bottom-line-two'>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='bottom-line-two-b'></article>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='bottom-line-two-c'></article>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='bottom-line-two-d'></article>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='bottom-line-two-c'></article>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+            <article className='bottom-line-two-e'></article>
+            <article className='bottom-line-two-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </article>
+          </section>
+          <section className='bottom-line-three'>
+            <div className='life'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='life'></div>
+          </section>
+        </article>
+      </main>
+
+      {/* miscelanious shapes positioned with absolute */}
+
+      {/* first line */}
+      <div className='shape-a'></div>
+      <div className='shape-b'></div>
+      <div className='shape-c'></div>
+      <div className='shape-d'></div>
+
+      {/* second line */}
+      <div className='shape-e'></div>
+      <div className='shape-f'></div>
+      <div className='shape-g'></div>
+      <div className='shape-h'></div>
+      <div className='shape-i'></div>
+      <div className='shape-j'></div>
+      <div className='shape-k'></div>
+      <div className='shape-l'></div>
+      <div className='shape-m'></div>
+      <div className='shape-n'></div>
+
+      {/* third line  */}
+      {/* top */}
+      <div className='shape-o'></div>
+      <div className='shape-p'></div>
+      <div className='shape-q'></div>
+      <div className='shape-r'></div>
+      <div className='shape-s'></div>
+      <div className='shape-t'></div>
+      <div className='shape-u'></div>
+      <div className='shape-v'></div>
+      <div className='shape-w'></div>
+      <div className='shape-x'></div>
+      <div className='shape-y'></div>
+      <div className='shape-z'></div>
+      <div className='shape-aa'></div>
+      <div className='shape-bb'></div>
+      {/* bottom */}
+      <div className='shape-o-b'></div>
+      <div className='shape-p-b'></div>
+      <div className='shape-q-b'></div>
+      <div className='shape-r-b'></div>
+      <div className='shape-s-b'></div>
+      <div className='shape-t-b'></div>
+      <div className='shape-u-b'></div>
+      <div className='shape-v-b'></div>
+      <div className='shape-w-b'></div>
+      <div className='shape-x-b'></div>
+      <div className='shape-y-b'></div>
+      <div className='shape-z-b'></div>
+      {/* middle dot container left  */}
+      <section className='dot-container-left'>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+      </section>
+      {/* middle dot container right */}
+      <section className='dot-container-right'>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+      </section>
+      {/* ghost house */}
+      <section className='ghost-house'></section>
+      {/* bottom middle shapes  */}
+      <div className="cc"></div>
+      <div className="dd"></div>
+      <div className="ee"></div>
+      <div className="ff"></div>
+      <div className="gg"></div>
+      <div className="hh"></div>
+      <div className="ii"></div>
+      <div className="jj"></div>
+      <div className="kk"></div>
+      <div className="ll"></div>
+      <div className="mm"></div>
+      <div className="nn"></div>
+      <div className="oo"></div>
+      <div className="pp"></div>
+    </>
+  )
+}
+
+export default App
