bb2a540b25c75bb2808142936e4e73e3a0ac92f4
Kris Kurzawa <kurzawa1@yahoo.com>
2025-01-24 15:14:03 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,164 @@ const App = () => {
   return (
     <>
       <header className='header'>
-        <h1 className='high-score'>HIGH SCORE</h1>
-        <h1>16440</h1>
+        {/* <h1 className='high-score'>HIGH SCORE</h1>
+        <h1>16440</h1> */}
+        <article className="high-score">
+          <section className="high">
+            <section className="h">
+              <div className="ha"></div>
+              <div className="hb"></div>
+              <div className="hc"></div>
+            </section>
+            <section className="i">
+              <div className="ia"></div>
+              <div className="ib"></div>
+              <div className="ic"></div>
+            </section>
+            <section className="g">
+              <div className="ga"></div>
+              <div className="gb"></div>
+              <div className="gc"></div>
+              <div className="gd"></div>
+              <div className="ge"></div>
+              <div className="gf"></div>
+              <div className="gh"></div>
+            </section>
+            <section className="h">
+              <div className="ha"></div>
+              <div className="hb"></div>
+              <div className="hc"></div>
+            </section>
+          </section>
+          <section className="score">
+            <section className="s">
+              <div className="sa"></div>
+              <div className="sb"></div>
+              <div className="sc"></div>
+              <div className="sd"></div>
+              <div className="se"></div>
+              <div className="sf"></div>
+              <div className="sg"></div>
+              <div className="sh"></div>
+            </section>
+            <section className="c">
+              <div className="ca"></div>
+              <div className="cb"></div>
+              <div className="cd"></div>
+              <div className="ce"></div>
+              <div className="cf"></div>
+              <div className="cg"></div>
+              <div className="ch"></div>
+            </section>
+            <section className="o">
+              <div className="oa"></div>
+              <div className="ob"></div>
+              <div className="oc"></div>
+              <div className="od"></div>
+            </section>
+            <section className="r">
+              <div className="ra"></div>
+              <div className="rb"></div>
+              <div className="rc"></div>
+              <div className="rd"></div>
+              <div className="re"></div>
+              <div className="rf"></div>
+              <div className="rg"></div>
+            </section>
+            <section className="e">
+              <div className="ea"></div>
+              <div className="eb"></div>
+              <div className="ec"></div>
+              <div className="ed"></div>
+              <div className="ef"></div>
+              <div className="eg"></div>
+            </section>
+          </section>
+        </article>
+        <article className="high-score-num">
+          <section className="one">
+            <div className="one-a"></div>
+            <div className="one-b"></div>
+            <div className="one-c"></div>
+          </section>
+          <section className="six">
+            <div className="six-a"></div>
+            <div className="six-b"></div>
+            <div className="six-c"></div>
+            <div className="six-d"></div>
+            <div className="six-e"></div>
+            <div className="six-f"></div>
+          </section>
+          <section className="four">
+            <div className="four-a"></div>
+            <div className="four-b"></div>
+            <div className="four-c"></div>
+            <div className="four-d"></div>
+            <div className="four-e"></div>
+            <div className="four-f"></div>
+            <div className="four-g"></div>
+          </section>
+          <section className="four">
+            <div className="four-a"></div>
+            <div className="four-b"></div>
+            <div className="four-c"></div>
+            <div className="four-d"></div>
+            <div className="four-e"></div>
+            <div className="four-f"></div>
+            <div className="four-g"></div>
+          </section>
+          <section className="zero">
+            <div className="zero-a"></div>
+            <div className="zero-b"></div>
+            <div className="zero-c"></div>
+            <div className="zero-d"></div>
+            <div className="zero-e"></div>
+            <div className="zero-f"></div>
+            <div className="zero-g"></div>
+            <div className="zero-h"></div>
+          </section>
+        </article>
       </header>
+      <div className="one-up">
+        <section className="one">
+          <div className="one-a"></div>
+          <div className="one-b"></div>
+          <div className="one-c"></div>
+        </section>
+        <section className="u">
+          <div className="u-a"></div>
+          <div className="u-b"></div>
+          <div className="u-c"></div>
+        </section>
+        <section className="p">
+          <div className="p-a"></div>
+          <div className="p-b"></div>
+          <div className="p-c"></div>
+          <div className="p-d"></div>
+        </section>
+      </div>
+      <div className="zero-zero">
+        <section className="zero">
+          <div className="zero-a"></div>
+          <div className="zero-b"></div>
+          <div className="zero-c"></div>
+          <div className="zero-d"></div>
+          <div className="zero-e"></div>
+          <div className="zero-f"></div>
+          <div className="zero-g"></div>
+          <div className="zero-h"></div>
+        </section>
+        <section className="zero">
+          <div className="zero-a"></div>
+          <div className="zero-b"></div>
+          <div className="zero-c"></div>
+          <div className="zero-d"></div>
+          <div className="zero-e"></div>
+          <div className="zero-f"></div>
+          <div className="zero-g"></div>
+          <div className="zero-h"></div>
+        </section>
+      </div>
       <main className='main'>
 
         {/* top 1/3 */}
@@ -241,7 +396,118 @@ const App = () => {
             </article>
           </section>
           <section className='bottom-line-three'>
-            <div className='life'></div>
+            <div className='life -mr-2'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='space'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='space-b'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='space'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='life -ml-2'></div>
+          </section>
+          {/* bottom line four */}
+          <section className="bottom-line-four">
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-e"></div>
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-d"></div>
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-c"></div>
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-d"></div>
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-e"></div>
+            <div className="bottom-line-four-a">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+          </section>
+
+          <section className="bottom-line-five">
+            <div className='dot-container-a'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='space-f'></div>
+            <div className='dot-container-b'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='space-g'></div>
+            <div className='dot-container-c'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className='space-f'></div>
+            <div className='dot-container-d'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+          </section>
+          <section className="bottom-line-six">
+            <div className="dot-container-e">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-i"></div>
+            <div className="dot-container-e">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-h"></div>
+            <div className="dot-container-e">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <div className="space-i"></div>
+            <div className="dot-container-e">
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+          </section>
+          <section className="bottom-line-seven">
             <div className='dot'></div>
             <div className='dot'></div>
             <div className='dot'></div>
@@ -262,11 +528,40 @@ const App = () => {
             <div className='dot'></div>
             <div className='dot'></div>
             <div className='dot'></div>
-            <div className='life'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+            <div className='dot'></div>
+          </section>
+        </article>
+
+        <article className="bottom">
+          <section className="pacs">
+            <div className='pacman-b'>
+              <div className="mouth"></div>
+            </div>
+            <div className='pacman-b'>
+              <div className="mouth"></div>
+            </div>
+          </section>
+          <section className="cherries">
+            <div className="cherry-a">
+              <div className="white-dot-a"></div>
+              <div className="white-dot-b"></div>
+            </div>
+            <div className="cherry-b">
+              <div className="white-dot-c"></div>
+              <div className="white-dot-d"></div>
+            </div>
           </section>
         </article>
       </main>
 
+      <div className="brown-dot"></div>
+      <div className="brown-line-a"></div>
+      <div className="brown-line-b"></div>
       {/* miscelanious shapes positioned with absolute */}
 
       {/* first line */}
@@ -357,6 +652,99 @@ const App = () => {
       <div className="nn"></div>
       <div className="oo"></div>
       <div className="pp"></div>
+
+      {/* bottom shapes  */}
+      <div className="qq"></div>
+      <div className="rr"></div>
+      <div className="ss"></div>
+      <div className="tt"></div>
+      <div className="uu"></div>
+      <div className="vv"></div>
+      <div className="ww"></div>
+      <div className="xx"></div>
+      <div className="yy"></div>
+      <div className="zz"></div>
+      <div className="aaa"></div>
+      <div className="bbb"></div>
+      <div className="ccc"></div>
+      <div className="ddd"></div>
+
+      {/* touch up shapes  */}
+      <div className="eee"></div>
+      <div className="fff"></div>
+      <div className="ggg"></div>
+      <div className="hhh"></div>
+      <div className="iii"></div>
+      <div className="jjj"></div>
+      <div className="kkk"></div>
+      <div className="lll"></div>
+      <div className="mmm"></div>
+
+      {/* ghosts */}
+      <div className="door"></div>
+      <div className="red">
+        <div className="red-eyes">
+          <div className="red-left-eye">
+            <div className="red-left-eyeball"></div>
+          </div>
+          <div className="red-right-eye">
+            <div className="red-right-eyeball"></div>
+          </div>
+        </div>
+        <div className="ghost-bottom-container">
+          <div className="ghost-tri"></div>
+          <div className="ghost-sq"></div>
+          <div className="ghost-tri"></div>
+        </div>
+      </div>
+      <div className="blue">
+        <div className="blue-eyes">
+          <div className="blue-left-eye">
+            <div className="blue-left-eyeball"></div>
+          </div>
+          <div className="blue-right-eye">
+            <div className="blue-right-eyeball"></div>
+          </div>
+        </div>
+        <div className="ghost-bottom-container">
+          <div className="ghost-tri"></div>
+          <div className="ghost-sq"></div>
+          <div className="ghost-tri"></div>
+        </div>
+      </div>
+      <div className="pink">
+        <div className="pink-eyes">
+          <div className="pink-left-eye">
+            <div className="pink-left-eyeball"></div>
+          </div>
+          <div className="pink-right-eye">
+            <div className="pink-right-eyeball"></div>
+          </div>
+        </div>
+        <div className="ghost-bottom-container">
+          <div className="ghost-tri"></div>
+          <div className="ghost-sq"></div>
+          <div className="ghost-tri"></div>
+        </div>
+      </div>
+      <div className="orange">
+        <div className="orange-eyes">
+          <div className="orange-left-eye">
+            <div className="orange-left-eyeball"></div>
+          </div>
+          <div className="orange-right-eye">
+            <div className="orange-right-eyeball"></div>
+          </div>
+        </div>
+        <div className="ghost-bottom-container">
+          <div className="ghost-tri"></div>
+          <div className="ghost-sq"></div>
+          <div className="ghost-tri"></div>
+        </div>
+      </div>
+      <div className='pacman'>
+        <div className="mouth"></div>
+      </div>
     </>
   )
 }
